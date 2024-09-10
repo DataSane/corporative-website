974a05516793e9ba3bd8201102ec3b2dd9f03653
kauansfr <kauan.franca@sptech.school>
2024-09-10 01:14:05 -0300

    Responsividade Finalizada

--- public/js/header-mobile.js
+++ public/js/header-mobile.js
@@ -1,14 +1,8 @@
 const btnOpenNavmob = document.querySelector('.btn-navmob');
-const mobileNavbar = document.querySelector('.nav-bar');
+const mobileMenu = document.querySelector('.mobile_menuHeader');
 
 btnOpenNavmob.addEventListener('click',()=>{
     btnOpenNavmob.classList.toggle('opened');
 
-    mobileNavbar.classList.toggle('active');
-
-    if(btnOpenNavmob.classList.contains('opened')){
-        btnOpenNavmob.textContent = 'X';
-    }else{
-        btnOpenNavmob.textContent = 'O';
-    }
+    mobileMenu.classList.toggle('active');
 })
